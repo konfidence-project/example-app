dc68f9459bf89f6ec2223916b43596e13ce29107
Armin Kramer <armin.kramer@sap.com>
2025-12-05 11:32:32 +0100

    fix

--- scenario-3/build-and-push-kustomizations.sh
+++ scenario-3/build-and-push-kustomizations.sh
@@ -8,5 +8,4 @@ SCENARIO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCENARIO_DIR/manifests/kustomizations
 
 # Push kustomizations
-oras push "$OCI_REPOSITORY_BASE/mysqldb:v1.0.0" ./mysqldb-v1/ --artifact-type application/vnd.kustomize.config.v1+yaml
 oras push "$OCI_REPOSITORY_BASE/ratings:v2.0.0" ./ratings-v2-mysql/ --artifact-type application/vnd.kustomize.config.v1+yaml
